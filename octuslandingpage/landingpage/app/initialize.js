a770988aea78e71a3f8a758234a163f3e40e1a89
Nicholas Teo <nicholasteock@gmail.com>
2014-09-25 14:56:26 +0800

    .

--- octuslandingpage/landingpage/app/initialize.js
+++
@@ -1,9 +0,0 @@
-var application = require('application')
-
-$(function() {
-    application.initialize()
-    Backbone.history.start()
-
-    // Initialize parallax scrolling here.
-    // s = skrollr.init();
-})
