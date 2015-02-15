57eddce4fd16dd000e5da85ab90406763e90f5f7
nicholasteock <nicholasteock@gmail.com>
2015-02-16 00:55:17 +0800

    .

--- louhei/js/app.js
+++ louhei/js/app.js
@@ -298,9 +298,10 @@ var application 	= require('application');
 // require('swiper');
 
 $(function() {
-	$('.loading-overlay').addClass('hide');
-	nick = application;
-	application.initialize();
+	$(document).ready( function() {
+		$('.loading-overlay').addClass('hide');
+		application.initialize();
+	});
 
 	// $('.app-stage').click(function() {
 	// 	if (!document.fullscreenElement &&    // alternative standard method
@@ -623,87 +624,6 @@ module.exports = {
 	enable 		: enable,
 	disable 	: disable
 };
-});
-
-require.register("swiper", function(exports, require, module) {
-var reqAnimationFrame = (function () {
-    return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
-        window.setTimeout(callback, 1000 / 60);
-    };
-})();
-
-var screen = document.querySelector(".ingredients-stage");
-var el = document.getElementById('mini-plate-container');
-
-// var START_X = Math.round(el.offsetWidth);
-// var START_Y = Math.round(el.offsetHeight);
-
-var START_X = 0;
-var START_Y = -1100;
-
-var miniPlateContainer = new Hammer(el);
-
-miniPlateContainer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-
-miniPlateContainer.on('swipe', onSwipe);
-
-var ticking = false;
-var transform;
-var timer;
-
-function resetElement() {
-    el.className = 'animate';
-    transform = {
-        translate: { x: START_X, y: START_Y },
-        scale: 1,
-        angle: 0,
-        rx: 0,
-        ry: 0,
-        rz: 0
-    };
-    requestElementUpdate();
-};
-
-function updateElementTransform() {
-    var value = [
-        'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0)',
-        'scale(' + transform.scale + ', ' + transform.scale + ')',
-        'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
-    ];
-
-    value = value.join(" ");
-    el.style.webkitTransform = value;
-    el.style.mozTransform = value;
-    el.style.transform = value;
-    ticking = false;
-};
-
-function requestElementUpdate() {
-    if(!ticking) {
-        reqAnimationFrame(updateElementTransform);
-        ticking = true;
-    }
-};
-
-function onSwipe(ev) {
-    var angle = 50;
-    transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
-    transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
-    transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
-
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-        resetElement();
-    }, 300);
-
-    requestElementUpdate();
-};
-
-resetElement();
-
-
-
-
 });
 
 
